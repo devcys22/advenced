505e09ded1406a443003f5a2dd3e44fc622c4f01
dev22 <ysoli36512@gmail.com>
2023-07-03 07:42:19 +0900

    Fix: 템플릿 메서드 패턴 단점 - 상속 --> 전략패턴

--- src/main/java/hello/advenced/app/v4/OrderControllerV4.java
+++ src/main/java/hello/advenced/app/v4/OrderControllerV4.java
@@ -18,6 +18,9 @@ public class OrderControllerV4 {
     public String request(String itemId){
 
         AbstractTemplate<String> template = new AbstractTemplate<String>(trace) {
+            //템플릿 메서드 패턴은 상속을 사용한다.
+            //따라서 상속의 단점을 그대로 안고간다. 구조적으로도 별도의 클래스나 익명 내부 클래스를 만들어야 한다.
+            //상속 단점을 제거할 수 있는 전략 패턴을 사용할 수 있다.
             @Override
             protected String call() {
                 orderService.orderItem(itemId);
